66348eadcc7957bdf8a2f6ddecf4240aa265485e
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-21 23:07:24 +0530

    Get simple request with url manipulation

--- app/src/main/java/com/github/bazukaa/nakshatra/retrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/github/bazukaa/nakshatra/retrofit/JsonPlaceHolderApi.java
@@ -1,13 +1,29 @@
 package com.github.bazukaa.nakshatra.retrofit;
 
+import android.content.Intent;
+
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
-//Get simple request
 public interface JsonPlaceHolderApi {
 
+    //Get simple request
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("id") Integer[] id,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
